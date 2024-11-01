dee09d9a4e1018a06166a2bd787ce9d612f3b925
Krzysztof Konieczko <kristopher1001@gmail.com>
2024-11-01 18:38:15 +0100

    functions relocated into form file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { useState } from 'react';
 import { Form } from './Form/index.js';
-import { currencies } from './Form/currencies';
 
 function App() {
 
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  }
   return (
-      <Form
-        result={result}
-        calculateResult={calculateResult} />
+      <Form />
   );
 }
 
