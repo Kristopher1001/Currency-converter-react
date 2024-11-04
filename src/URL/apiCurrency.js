6ddc8bb857155fc7552efb91a5e66650f4e43c0e
Krzysztof Konieczko <kristopher1001@gmail.com>
2024-11-04 16:42:16 +0100

    changed souce API

--- src/URL/apiCurrency.js
+++ src/URL/apiCurrency.js
@@ -1,3 +1,3 @@
-// export const CURRENCY_API = "https://api.currencyapi.com/v3/historical?apikey=cur_live_zQcHw9IlwTE54nyVdymxjgvRdntXybJQHpaYc5aa&currencies&base_currency=PLN&date=2024-11-01";
+export const CURRENCY_API = "https://api.currencyapi.com/v3/historical?apikey=cur_live_zQcHw9IlwTE54nyVdymxjgvRdntXybJQHpaYc5aa&currencies&base_currency=PLN&date=2024-11-01";
 
-export const CURRENCY_API = "currencies.json";
+// export const CURRENCY_API = "currencies.json";
